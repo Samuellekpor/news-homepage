557ba787ccec48b89a302b4cd78bd879b4599cae
Samuellekpor <samuellekpor@gmail.com>
2023-07-12 15:28:05 +0000

    Correct add styling to the mobile menu

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
+const menuContainer = document.querySelector(".menuContainer");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
+    menuContainer.classList.remove("dark");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
     menu.classList.add("showMenu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
+    menuContainer.classList.add("dark");
   }
 }
 
